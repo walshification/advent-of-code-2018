7cd76771151be096d298a2288a597c2670c794f3
Chris Walsh <cwalsh@leapfrogonline.com>
2018-12-04 16:21:20 -0600

    solves part 2

--- solutions/chronal_calibration.py
+++ solutions/chronal_calibration.py
@@ -1,3 +1,5 @@
+import itertools
+
 import yaml
 
 
@@ -5,8 +7,21 @@ def adjust_frequency(modulations):
     return sum(int(modulation) for modulation in modulations)
 
 
+def cycle_for_repeat(modulations):
+    frequencies = [0]
+    total = 0
+    for modulation in itertools.cycle(modulations):
+        delta = int(modulation)
+        total += delta
+        if total in frequencies:
+            return total
+        else:
+            frequencies.append(total)
+
+
 if __name__ == "__main__":
     with open('inputs/chronal_calibration.yaml', 'r') as modulations:
         test_input = yaml.safe_load(modulations)
-    adjusted = adjust_frequency(test_input.split())
-    print('Part One:', adjusted)
+
+    print('Part One:', adjust_frequency(test_input.split()))
+    print('Part Two:', cycle_for_repeat(test_input.split()))
--- tests/test_chronal_calibration.py
+++ tests/test_chronal_calibration.py
@@ -1,6 +1,6 @@
 import unittest
 
-from solutions.chronal_calibration import adjust_frequency
+from solutions.chronal_calibration import adjust_frequency, cycle_for_repeat
 
 
 class ChronalCalibrationTests(unittest.TestCase):
@@ -15,3 +15,15 @@ class ChronalCalibrationTests(unittest.TestCase):
     def test_adjusts_both_as_needed(self):
         result = adjust_frequency(["+1", "-2", "+3", "+1"])
         self.assertEqual(result, 3)
+
+    def test_cycles_through_modulations_till_it_hits_a_repeat(self):
+        result = cycle_for_repeat(["+3", "+3", "+4", "-2", "-4"])
+        self.assertEqual(result, 10)
+
+    def test_another_example(self):
+        result = cycle_for_repeat(["-6", "+3", "+8", "+5", "-6"])
+        self.assertEqual(result, 5)
+
+    def test_one_more_example(self):
+        result = cycle_for_repeat(["+7", "+7", "-2", "-7", "-4"])
+        self.assertEqual(result, 14)
