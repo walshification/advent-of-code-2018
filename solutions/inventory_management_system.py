cdb37b9494f73015666278acb36211c834af193a
Christopher Walsh <walshification@gmail.com>
2018-12-08 12:46:24 -0600

    solves part 2

--- solutions/inventory_management_system.py
+++ solutions/inventory_management_system.py
@@ -18,18 +18,32 @@ def count_treble_scans(box_ids):
 
 
 def count_scan(box_id, specific_count):
-    if has_specific_count(box_id, specific_count):
+    counter = Counter(box_id)
+    if any(count == specific_count for _, count in counter.items()):
         return 1
     return 0
 
 
-def has_specific_count(box_id, count_in_question):
-    counter = Counter(box_id)
-    return any(count == count_in_question for _, count in counter.items())
+def filter_ids(box_ids):
+    for index, box_id in enumerate(box_ids):
+        for i in range(index+1, len(box_ids)):
+            next_id = box_ids[i % len(box_ids)]
+            if len(id_union(box_id, next_id)) == len(box_id) - 1:
+                return (box_id, next_id)
+    return ()
+
+
+def id_union(first_id, second_id):
+    matches = []
+    for first_char, second_char in zip(first_id, second_id):
+        if first_char == second_char:
+            matches.append(first_char)
+    return "".join(matches)
 
 
 if __name__ == "__main__":
-    with open("inputs/inventory_management_system.yaml", "r") as box_ids:
-        test_input = yaml.safe_load(box_ids)
+    with open("inputs/inventory_management_system.yaml", "r") as test_input:
+        box_ids = yaml.safe_load(test_input).split()
 
-    print("Part One:", calculate_checksum(test_input.split()))
+    print("Part One:", calculate_checksum(box_ids))
+    print("Part Two:", id_union(*filter_ids(box_ids)))
--- tests/test_inventory_management_system.py
+++ tests/test_inventory_management_system.py
@@ -4,10 +4,12 @@ from solutions.inventory_management_system import (
     calculate_checksum,
     count_duple_scans,
     count_treble_scans,
+    filter_ids,
+    id_union,
 )
 
 
-class InventoryManagementSystemTests(unittest.TestCase):
+class CalculateCheckSumsTests(unittest.TestCase):
     def test_count_duple_scans_returns_zero_if_scans_dont_have_letters_the_same(self):
         count = count_duple_scans(["asdf"])
         self.assertEqual(count, 0)
@@ -32,3 +34,33 @@ class InventoryManagementSystemTests(unittest.TestCase):
         box_ids = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
         checksum = calculate_checksum(box_ids)
         self.assertEqual(checksum, 12)
+
+
+class IdUnionTests(unittest.TestCase):
+    def test_returns_an_empty_string_if_no_characters_match(self):
+        match = id_union("asdf", "qwer")
+        self.assertEqual(match, "")
+
+    def test_returns_a_single_matching_letter_if_it_matches(self):
+        match = id_union("a", "a")
+        self.assertEqual(match, "a")
+
+    def test_skips_characters_that_do_not_match(self):
+        match = id_union("bb", "bd")
+        self.assertEqual(match, "b")
+
+    def test_matches_against_any_number_of_characters(self):
+        match = id_union("fghij", "fguij")
+        self.assertEqual(match, "fgij")
+
+    def test_filter_returns_empty_tuple_if_box_ids_are_totally_different(self):
+        matching_ids = filter_ids(["asdf", "qwer"])
+        self.assertEqual(matching_ids, ())
+
+    def test_filter_returns_box_ids_if_they_are_just_one_character_off_each_other(self):
+        matching_ids = filter_ids(["asdf", "awdf"])
+        self.assertEqual(matching_ids, ("asdf", "awdf"))
+
+    def test_filter_compares_one_id_to_all_others(self):
+        matching_ids = filter_ids(["asdf", "qwer", "awdf"])
+        self.assertEqual(matching_ids, ("asdf", "awdf"))
